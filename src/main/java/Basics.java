d05a2462bb81e52b87dcabfaf5c6efbfcfd0c5ac
Nasim Bondar Sahebi <sogol.sahebi@mail.utoronto.ca>
2022-09-22 18:20:05 -0400

    completed :)

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -61,7 +62,7 @@ public class Basics {
         /*
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
-         */
+         */int my_variable = 100;
 
 
 
@@ -99,6 +100,10 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
+
 
 
     }
@@ -142,10 +147,15 @@ public class Basics {
          */
         StringBuilder ret = new StringBuilder();
 
-        // Fill in the rest of the body here
-
-        return ret.toString();
+        String[] words = to_split.split("\\s");
+        for(String word: words){
+            ret.append(word.charAt(0));
+        }
+         return ret.toString();
     }
+
+
+
 
     /**
      * 7. Below is a method that takes in an integer array (similar to a list
@@ -169,10 +179,24 @@ public class Basics {
          *                .length attribute (e.g. arr.length)
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
+         *
+         *  Return the sum of all integers at odd indices in the given array.
+         * If there are no items at odd indices (e.g. a list with 0 or 1 elements),
+         * return 0.
          */
+        if(arr.length > 1){
+            for(int i = 0 ; i < arr.length; i++){
+                if( i % 2 != 0){
+                    current_sum += arr[i];
+                }
+            }
+        }
+
 
         return current_sum;
     }
+
+
 
 
     /*
